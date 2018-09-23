868074489dc6db9878a01304f2f694e40410235b
Shavrin <kacper.olek@gmail.com>
2018-09-23 17:19:10 +0200

    backup every 12h

--- server.js
+++ server.js
@@ -57,8 +57,8 @@ function startBot() {
 	setTimeout(function () {
 		logger.info(`KILLING BOT`);
 		botProcess.kill();
-	}, 1800000);
-
+	}, 43200000);
+	//12hours
 	function backupSpawn() {
 		logger.info(`BACKUP RUNNING------------------------------------`);
 		logger.info("\n" + execSync('sh backup.sh').toString());
